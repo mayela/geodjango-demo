10e0f4ff275ce8a87a5332160a52ecdbe830af07
Maricela Sanchez <mayela0x14@gmail.com>
2018-02-01 17:12:31 -0600

    README added. A query example added

---
+++ README.md
@@ -0,0 +1,7 @@
+# GeoDjango: pequeña introducción
+
+Demo de GeoDjango.
+
+Enlaces importantes:
+
+- [Presentación](https://slides.com/mayela0x14/geodjango)
---
+++ hospitals/utils.py
@@ -0,0 +1,15 @@
+from django.contrib.gis.geos import GEOSGeometry
+from django.contrib.gis.measure import D
+
+from hospitals.models import Hospital
+
+def closest_stops_by_location(latitude, longitude, distance=5):
+    point_wkt = 'POINT({} {})'.format(longitude, latitude)
+    point = GEOSGeometry(point_wkt, srid=4326)
+    hospitals = Hospital.objects.filter(
+        location__distance_lte=(
+            point,
+            D(km=distance)
+            )
+        )
+    return hospitals
